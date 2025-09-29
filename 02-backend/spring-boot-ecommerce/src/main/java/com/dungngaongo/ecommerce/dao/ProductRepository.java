2147682077c1b4b78a62db0d86e2a8fb7bea2eef
Nguyen Tan Dung <09tandungnguyen@gmail.com>
2025-09-30 00:03:28 +0700

    init project

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/dungngaongo/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.dungngaongo.ecommerce.dao;
+
+import com.dungngaongo.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
