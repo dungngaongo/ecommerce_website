6ad4be8bc3759c1c93007a50e29f68bc55eb158e
Nguyen Tan Dung <09tandungnguyen@gmail.com>
2025-10-07 15:57:43 +0700

    create product class and service

--- 03-frontend/angular-ecommerce/src/app/app-module.ts
+++ 03-frontend/angular-ecommerce/src/app/app-module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { ProductList } from './components/product-list/product-list';
+import { HttpClientModule } from '@angular/common/http';
+import { Product } from './services/product';
 
 @NgModule({
   declarations: [
@@ -12,10 +14,12 @@ import { ProductList } from './components/product-list/product-list';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    Product
   ],
   bootstrap: [App]
 })
